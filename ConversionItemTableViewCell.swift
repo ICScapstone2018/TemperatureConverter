36ccae6b92237a9d7fa49433f2152cf99b13f1c5
Kerensa E Mattison <mattison@icsmac06.local>
2018-02-22 12:58:02 -0800

    starting table views

---
+++ ConversionItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ConversionItemTableViewCell.swift
+//  TemperatureConverter
+//
+//  Created by  Kerensa E Mattison on 2018-02-22.
+//  Copyright © 2018  Kerensa E Mattison. All rights reserved.
+//
+
+import UIKit
+
+class ConversionItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
